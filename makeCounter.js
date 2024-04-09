afb11af81eeb96ed8cc8d68e50840087941b5311
Kevin Castro <cas20057@byui.edu>
2024-04-08 18:52:11 -0600

    solved the makeCounter challenge

--- makeCounter.js
+++ makeCounter.js
@@ -16,6 +16,16 @@
  * @param {number} initialValue
  * @return {Function}
  */
-export default function makeCounter(initialValue = 0) {
-  throw 'Not implemented';
+function makeCounter(initialValue = 0) {
+  //   let count = initialValue;
+
+  //   return () => {
+  //     return count++;
+  //   };
+
+  return () => initialValue++;
 }
+
+makeCounter();
+makeCounter();
+makeCounter();
