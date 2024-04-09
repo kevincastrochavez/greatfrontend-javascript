73420cbad809c92d11385c0a430038deeec94226
Kevin Castro <cas20057@byui.edu>
2024-04-09 14:32:01 -0600

    solved the number of arguments problem

---
+++ numberOfArguments.js
@@ -0,0 +1,21 @@
+// Implement a function numberOfArguments, to return the number of arguments it was called with. Note that this value is the actual number of arguments, which can be more or less than the defined parameter count (which is determined by Function.prototype.length).
+
+// P.S. There's no practical use for this function. However, it is useful to know how to determine the number of arguments, which can be useful for questions that require writing variadic functions like Classnames and Curry II.
+
+// numberOfArguments(); // 0
+// numberOfArguments(1); // 1
+// numberOfArguments(2, 3); // 2
+// numberOfArguments('a', 'b', 'c'); // 3
+
+/**
+ * @param {...any} args
+ * @return {number}
+ */
+function numberOfArguments() {
+  return arguments.length;
+}
+
+numberOfArguments();
+numberOfArguments(1);
+numberOfArguments(2, 3);
+numberOfArguments('a', 'b', 'c');
